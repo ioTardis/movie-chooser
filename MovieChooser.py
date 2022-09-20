e5feb21258fe44b43f4f5bc0a54340912f954740
Izabella Golovina <ia.golovina00@gmail.com>
2022-09-20 13:16:58 +0200

    brush up the code #6

--- MovieChooser.py
+++ MovieChooser.py
@@ -2,10 +2,10 @@ import json
 
 import PySimpleGUI as sg
 from dialogwindows import (
-    add_movie_window, 
-    choose_actor_window, 
-    choose_director_window, 
-    edit_list_window, 
+    add_movie_window,
+    choose_actor_window,
+    choose_director_window,
+    edit_list_window,
     validate_number,
 )
 from randomizing import duration_movie, random_from_list
